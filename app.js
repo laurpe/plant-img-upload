fddb32e05ca63637f9c4602a6f62ae361d926ebf
laurpe <laura.perttila@edu.bc.fi>
2022-06-30 13:35:30 +0300

    index for local development

---
+++ app.js
@@ -0,0 +1,44 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
+const bodyParser = require("body-parser");
+const { Readable } = require("stream");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_S3_ACCESS_KEY_SECRET,
+});
+
+app.post(
+    "/api/upload",
+    bodyParser.raw({ type: ["image/jpeg", "image/png"], limit: "5mb" }),
+    (req, res) => {
+        const fileStream = Readable.from(req.body);
+
+        const imgName = uuidv4();
+
+        const params = {
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Key: imgName,
+            Body: fileStream,
+        };
+
+        s3.upload(params, (error, data) => {
+            if (error) {
+                res.send({ message: "Could not upload image", error: error });
+            } else {
+                res.send({ imgName: imgName });
+            }
+        });
+    }
+);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,45 +1,5 @@
-const dotenv = require("dotenv");
-const express = require("express");
-const AWS = require("aws-sdk");
-const { v4: uuidv4 } = require("uuid");
-const bodyParser = require("body-parser");
-const { Readable } = require("stream");
-const cors = require("cors");
 const serverless = require("serverless-http");
 
-dotenv.config();
-
-const app = express();
-
-app.use(cors());
-
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_S3_ACCESS_KEY_SECRET,
-});
-
-app.post(
-    "/api/upload",
-    bodyParser.raw({ type: ["image/jpeg", "image/png"], limit: "5mb" }),
-    (req, res) => {
-        const fileStream = Readable.from(req.body);
-
-        const imgName = uuidv4();
-
-        const params = {
-            Bucket: process.env.AWS_S3_BUCKET_NAME,
-            Key: imgName,
-            Body: fileStream,
-        };
-
-        s3.upload(params, (error, data) => {
-            if (error) {
-                res.send({ message: "Could not upload image", error: error });
-            } else {
-                res.send({ imgName: imgName });
-            }
-        });
-    }
-);
+const app = require("./app.js");
 
 module.exports.handler = serverless(app);
